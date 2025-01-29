6891bbbd29816cb9cd80ee524d5719c71023e4d2
ManojGowda2006 <manojgowdan807@gmail.com>
2025-01-29 12:48:40 +0530

    Milestone 9

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import LoginPage from './pages/LoginPage'
 import SignUpPage  from './pages/SignUpPage'
 import HomePage from './pages/HomePage'
+import CreatePage from './pages/createPage'
 import {Routes,Route,BrowserRouter} from 'react-router-dom'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='/' element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path='/create-product' element={<CreatePage/>}/>
       </Routes>
     </BrowserRouter>
     </>
